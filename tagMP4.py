11abca642bf90ff08459e417d56cd7e94ed97038
Sean Fleming <smenus@me.com>
2013-02-06 18:04:46 +0000

    Refactored tagMP4, untested

--- tagMP4.py
+++ tagMP4.py
@@ -26,100 +26,136 @@ import patterns
 
 class Episode_Tags:
     def __init__(self, tvdb_id, season_num, episode_num, file):
-        self.tags = []
         self.file = file
+        self.tvdb_id = tvdb_id
+        self.season_num = season_num
+        self.episode_num = episode_num
         self.artwork_path = None
         self.airdate = None
+        self.tvdb_show = None
+        self.tvdb_episode = None
+        self.itunes_season = None
+        self.itunes_episode = None
 
-        print ' - Fetching information'
+    def _fetch_tvdb(self):
+        print ' - Getting TheTVDB metadata'
 
         tvdb_instance = Tvdb(cache=True, banners=True, actors=True)
+        
         try:
-            tvdb_show = tvdb_instance[tvdb_id]
-            tvdb_episode = tvdb_show[season_num][episode_num]
-
-            itunes_episodes = itunes.search_episode(tvdb_show['seriesname'] + ' ' + tvdb_episode['episodename'])
-            if len(itunes_episodes) > 0:
-                itunes_episode = itunes_episodes[0]
-                itunes_season = itunes_episode.get_album()
-                print ' - iTunes episode information found'
-            else:
-                itunes_episode = None
-                itunes_season = itunes.search_season(tvdb_show['seriesname'] + ', Season ' + str(season_num))
-                print ' - No iTunes episode information found'
-                if len(itunes_season) == 0:
-                    itunes_season = None
-                    print ' - No iTunes season information found'
-                else:
-                    itunes_season = itunes_season[0]
-                    print ' - iTunes season information found'
+            self.tvdb_show = tvdb_instance[tvdb_id]
+            self.tvdb_episode = self.tvdb_show[season_num][episode_num]
         except tvdb_error, errormsg:
-            print ' @ Error: Could not fetch episode information - %s' % errormsg
-            itunes_episode = None
-            tvdb_show = None
+            print ' - Warning - Could not fetch TheTVDB metadata - %s' % errormsg
+            self.tvdb_show = None
+            self.tvdb_episode = None
+            return
 
-        self.tags.append({'TVSeasonNum': str(season_num)})
-        self.tags.append({'TVEpisodeNum': str(episode_num)})
-        self.tags.append({'disk': '1/1'})
-        self.tags.append({'stik': 'TV Show'})
 
-        self.get_local_artwork()
+    def _fetch_itunes(self):
+        if self.tvdb_show is not None and self.tvdb_episode is not None:
+            print ' - Getting iTunes metadata from TheTVDB metadata'
 
-        if tvdb_show is not None and tvdb_episode is not None:
-            self.tags.append({'TVNetwork': tvdb_show['network']})
-            self.tags.append({'TVEpisode': tvdb_episode['productioncode']})
+            seriesname = re.sub("(.*?) \(\d\d\d\d\)", "\\1", self.tvdb_show['seriesname'])
+            itunes_episodes = itunes.search_episode(seriesname + ' ' + self.tvdb_episode['episodename'])
 
-        if itunes_episode is not None:
-            self.tags.append({'TVShowName': itunes_episode.get_artist().get_name()})
-            self.tags.append({'artist': itunes_episode.get_artist().get_name()})
-            self.tags.append({'title': itunes_episode.get_name()})
-            self.tags.append({'album': itunes_season.get_name()})
-            self.tags.append({'genre': itunes_episode.get_genre()})
-            self.tags.append({'tracknum': str(episode_num) + '/' + str(itunes_season.get_track_count())})
-            self.tags.append({'year': itunes_episode.get_release_date_raw()})
-            self.airdate = itunes_episode.get_release_date_raw()
-            self.tags.append({'copyright': itunes_season.get_copyright()})
-            self.tags.append({'advisory': ('clean' if itunes_episode.get_explicitness() == 'notExplicit' else 'explicit')})
-            self.tags.append({'cnID': str(itunes_episode.get_id())})
-            self.tags.append({'contentRating': itunes_episode.get_content_rating()})
-            if self.artwork_path is None:
-                self.get_itunes_artwork(itunes_season)
-            self.tags.append({'description': itunes_episode.get_long_description()[:252] + (itunes_episode.get_long_description()[252:] and '...')})
-            self.tags.append({'longdesc': itunes_episode.get_long_description()})
-            self.tags.append({'comment': 'Tagged with iTunes metadata'})
-        elif tvdb_show is not None:
-            self.tags.append({'TVShowName': tvdb_show['seriesname']})
-            self.tags.append({'artist': tvdb_show['seriesname']})
-            self.tags.append({'title': tvdb_episode['episodename']})
-            self.tags.append({'album': tvdb_show['seriesname'] + ', Season ' + str(season_num)})
-            self.tags.append({'genre': tvdb_show['genre'].strip('|').split('|')[0]})
-            self.tags.append({'tracknum': str(episode_num) + '/' + str(len(tvdb_show[season_num]))})
-            self.tags.append({'year': tvdb_episode['firstaired']})
-            self.airdate = tvdb_episode['firstaired']
-            self.tags.append({'cnID': tvdb_episode['id']})
-            if self.artwork_path is None:
-                if itunes_season is None:
-                    self.get_tvdb_artwork(tvdb_show, season_num)
-                else:
-                    self.get_itunes_artwork(itunes_season)
-            self.tags.append({'description': tvdb_episode['overview'][:252] + (tvdb_episode['overview'][252:] and '...')})
-            self.tags.append({'longdesc': tvdb_episode['overview']})
-            self.tags.append({'comment': 'Tagged with TheTVDB metadata'})
+            if len(itunes_episodes) > 0:
+                self.itunes_episode = itunes_episodes[0]
+                self.itunes_season = self.itunes_episode.get_album()
+            else:
+                print ' - No iTunes metadata found from TheTVDB metadata'
+
+        if self.itunes_episode is None or self.itunes_season is None:
+            print ' - Getting iTunes metadata from filename'
+
+            fp = Filename_Parser(self.file)
+            seriesname = re.sub("(.*?) \(\d\d\d\d\)", "\\1", fp.parse())
+
+            self.itunes_season = itunes.search_season(seriesname + ', Season ' + str(self.season_num))
+            itunes_episodes = self.itunes_season.get_tracks()
+
+            for ep in itunes_episodes:
+                if ep.number == int(self.episode_num):
+                    self.itunes_episode = ep
+                    break
+
+            if self.itunes_episode is None:
+                print ' - No iTunes metadata found from filename'
+                self.itunes_season = None
+
+
+    def _get_metadata(self):
+        self._fetch_tvdb()
+        self._fetch_itunes()
+        
+        print ' - Get metadata from discovered source'
+
+        tags = []
+
+        tags.append({'TVSeasonNum': str(self.season_num)})
+        tags.append({'TVEpisodeNum': str(self.episode_num)})
+        tags.append({'disk': '1/1'})
+        tags.append({'stik': 'TV Show'})
+        tags.append({'artwork': self._get_artwork()})
+
+        # Options only TheTVDB has
+        if self.tvdb_show is not None and self.tvdb_episode is not None:
+            tags.append({'TVNetwork': self.tvdb_show['network']})
+            tags.append({'TVEpisode': self.tvdb_episode['productioncode']})
+
+        # Options only iTunes has
+        if self.itunes_season is not None and self.itunes_episode is not None:
+            tags.append({'copyright': self.itunes_season.get_copyright()})
+            tags.append({'advisory': ('clean' if self.itunes_episode.get_explicitness() == 'notExplicit' else 'explicit')})
+            tags.append({'contentRating': self.itunes_episode.get_content_rating()})
+        
+        # Can be set from either iTunes or TheTVDB
+        if self.itunes_season is not None and self.itunes_episode is not None:
+            tags.append({'TVShowName': self.itunes_episode.get_artist().get_name()})
+            tags.append({'artist': self.itunes_episode.get_artist().get_name()})
+            tags.append({'title': self.itunes_episode.get_name()})
+            tags.append({'album': self.itunes_season.get_name()})
+            tags.append({'genre': self.itunes_episode.get_genre()})
+            tags.append({'tracknum': str(self.episode_num) + '/' + str(self.itunes_season.get_track_count())})
+            tags.append({'year': self.itunes_episode.get_release_date_raw()})
+            tags.append({'cnID': str(self.itunes_episode.get_id())})
+            tags.append({'description': self.itunes_episode.get_long_description()[:254] + (self.itunes_episode.get_long_description()[254:] and '\u2026')})
+            tags.append({'longdesc': self.itunes_episode.get_long_description()})
+            tags.append({'comment': 'Tagged with iTunes metadata'})
+            self.airdate = self.itunes_episode.get_release_date_raw()
+        elif self.tvdb_show is not None and self.tvdb_episode is not None:
+            tags.append({'TVShowName': self.tvdb_show['seriesname']})
+            tags.append({'artist': self.tvdb_show['seriesname']})
+            tags.append({'title': self.tvdb_episode['episodename']})
+            tags.append({'album': self.tvdb_show['seriesname'] + ', Season ' + str(self.season_num)})
+            tags.append({'genre': self.tvdb_show['genre'].strip('|').split('|')[0]})
+            tags.append({'tracknum': str(self.episode_num) + '/' + str(len(self.tvdb_show[self.season_num]))})
+            tags.append({'year': self.tvdb_episode['firstaired']})
+            tags.append({'cnID': self.tvdb_episode['id']})
+            tags.append({'description': self.tvdb_episode['overview'][:254] + (self.tvdb_episode['overview'][254:] and '\u2026')})
+            tags.append({'longdesc': self.tvdb_episode['overview']})
+            tags.append({'comment': 'Tagged with TheTVDB metadata'})
+            self.airdate = self.tvdb_episode['firstaired']
         else:
-            fp = Filename_Parser(file)
+            fp = Filename_Parser(self.file)
             seriesname = fp.parse()
-            self.tags.append({'TVShowName': seriesname})
-            self.tags.append({'artist': seriesname})
-            self.tags.append({'title': os.path.splitext(os.path.split(file)[1])[0]})
-            self.tags.append({'album': seriesname + ', Season ' + str(season_num)})
-            self.tags.append({'tracknum': str(episode_num)})
+            tags.append({'TVShowName': seriesname})
+            tags.append({'artist': seriesname})
+            tags.append({'title': os.path.splitext(os.path.split(self.file)[1])[0]})
+            tags.append({'album': seriesname + ', Season ' + str(self.season_num)})
+            tags.append({'tracknum': str(self.episode_num)})
 
-        if config.get('tagMP4', 'add_people') == 'True' and tvdb_show is not None:
-            print ' - Adding cast and crew information'
-            self.xml = self.gen_XML(tvdb_show, tvdb_episode)
+        # Check file for HDness
+        print ' - Scanning video to check HDness'
+
+        c = Converter(config.get('paths', 'ffmpeg'), config.get('paths', 'ffprobe'))
+        info = c.probe(self.file)
+        tags.append({'hdvideo': '1' if (info.video.video_height >= 700 or info.video.video_width >= 1260) else '0'})
+
+        return tags
 
 
-    def gen_XML(self, tvdb_show, tvdb_episode):
+    def _gen_XML(self):
         header = '<?xml version="1.0" encoding="UTF-8"?>\n<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">\n<plist version="1.0">\n<dict>'
         castheader = '\n  <key>cast</key>\n  <array>'
         directorheader = '\n  <key>directors</key>\n  <array>'
@@ -133,7 +169,7 @@ class Episode_Tags:
         output = StringIO.StringIO()
         output.write(header)
 
-        directors = tvdb_episode['director']
+        directors = self.tvdb_episode['director']
         if directors is not None:
             output.write(directorheader)
             for director in directors.strip('|').split('|'):
@@ -143,7 +179,7 @@ class Episode_Tags:
                     output.write(namefooter)
             output.write(subfooter)
 
-        writers = tvdb_episode['writer']
+        writers = self.tvdb_episode['writer']
         if writers is not None:
             output.write(writerheader)
             for writer in writers.strip('|').split('|'):
@@ -153,7 +189,7 @@ class Episode_Tags:
                     output.write(namefooter)
             output.write(subfooter)
 
-        actors = tvdb_show['_actors']
+        actors = self.tvdb_show['_actors']
         if actors is not None:
             output.write(castheader)
             for actor in actors:
@@ -171,62 +207,56 @@ class Episode_Tags:
         return xml
 
 
-    def get_local_artwork(self):
+    def _get_artwork(self):
         artwork = os.path.join(os.path.dirname(self.file), config.get('tagMP4', 'artwork_filename'))
+        artwork_path = None
 
         for ext in ['.png', '.jpg', '.jpeg']:
             if os.path.exists(artwork + ext):
-                print ' - Local artwork found - ' + artwork + ext
-                self.artwork_path = artwork + ext
+                print ' - Local artwork found'
+                artwork_path = artwork + ext
+
+        if artwork_path is None and self.itunes_season is not None:
+            artwork100 = self.itunes_season.get_artwork()['100']
+            artwork600 = artwork100.replace('100x100', '600x600')
+            print ' - iTunes artwork found'
+            artwork_path = urllib.urlretrieve(artwork600)[0]
+
+        if artwork_path is None and self.tvdb_show is not None:
+            banners = self.tvdb_show['_banners']['season']['season']
+            banners = [banners[banner] for banner in banners if (banners[banner]['season'] == str(self.season_num) and banners[banner]['language'] == 'en')]
+            banners = sorted(banners, key = self._sort_banners_by_rating, reverse = True)
+            artwork_path = urllib.urlretrieve(banners[0]['_bannerpath'])[0]
+
+        return artwork_path
 
 
-    def get_itunes_artwork(self, itunes_season):
-        artwork100 = itunes_season.get_artwork()['100']
-        artwork600 = artwork100.replace('100x100', '600x600')
-        self.retrieve_artwork(artwork600)
-
-
-    def get_tvdb_artwork(self, tvdb_show, season_num):
-        banners = tvdb_show['_banners']['season']['season']
-        banners = [banners[banner] for banner in banners if (banners[banner]['season'] == str(season_num) and banners[banner]['language'] == 'en')]
-        banners = sorted(banners, key = self.sort_banners_by_rating, reverse = True)
-        self.retrieve_artwork(banners[0]['_bannerpath'])
-
-
-    def retrieve_artwork(self, url):
-        self.artwork_path = urllib.urlretrieve(url)[0]
-
-
-    def sort_banners_by_rating(self, k):
+    def _sort_banners_by_rating(self, k):
         if 'rating' in k:
             return float(k['rating'])
         else:
             return 0
 
 
-    def set_hd(self, hd):
-        self.tags.append({'hdvideo': hd})
-
-
     def get_airdate(self):
         return self.airdate
 
 
-    def get_tags(self):
-        command = []
-        for tag in self.tags:
+    def get_commands(self):
+        commands = []
+
+        for tag in self._get_metadata():
             if tag.values()[0] is not None:
-                command.append('--' + tag.keys()[0])
-                command.append(tag.values()[0])
-        if hasattr(self, 'artwork_path'):
-            command.append('--artwork')
-            command.append(self.artwork_path)
-        if config.get('tagMP4', 'add_people') == 'True' and hasattr(self, 'xml'):
-            command.append('--rDNSatom')
-            command.append(self.xml)
-            command.append('name=iTunMOVI')
-            command.append('domain=com.apple.iTunes')
-        return command
+                commands.append('--' + tag.keys()[0])
+                commands.append(tag.values()[0])
+
+        if config.get('tagMP4', 'add_people') == 'True' and self.tvdb_show is not None:
+            commands.append('--rDNSatom')
+            commands.append(self._gen_XML())
+            commands.append('name=iTunMOVI')
+            commands.append('domain=com.apple.iTunes')
+
+        return commands
 
 
 
@@ -235,26 +265,27 @@ class MP4_Tagger:
         self.file = file
         self.tags = tags
 
-        print ' - Scanning video to check HDness'
-        c = Converter(config.get('paths', 'ffmpeg'), config.get('paths', 'ffprobe'))
-        info = c.probe(self.file)
-        self.tags.set_hd('1' if (info.video.video_height >= 700 or info.video.video_width >= 1260) else '0')
-
 
     def write(self):
         print ' - Writing tags to file'
+
         environment = os.environ.copy()
         environment['PIC_OPTIONS'] = 'removeTempPix'
+
         command = [config.get('paths', 'atomicparsley'), self.file, '--overWrite', '--metaEnema', '--artwork', 'REMOVE_ALL']
-        command.extend(self.tags.get_tags())
+        command.extend(self.tags.get_commands())
+
         if config.get('general', 'debug') == 'True':
             print command
+        
         subprocess.check_output(command, env = environment)
 
 
     def update_times(self):
         print ' - Updating file creation and modification times'
+
         airdate = self.tags.get_airdate()
+        
         if airdate is not None:
             airdate = dateutil.parser.parse(airdate)
             file_date = time.mktime(airdate.timetuple())
@@ -326,7 +357,6 @@ class Filename_Parser:
                     errormsg, cpattern))
             else:
                 self.compiled_regexs.append(cregex)
-
 
 
 
